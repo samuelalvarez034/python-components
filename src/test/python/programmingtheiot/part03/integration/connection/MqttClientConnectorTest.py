548157002df8474515bdbf1b609aaccb95ee6f65
samuelalvarez034 <you@example.com>
2025-03-27 09:23:39 +0100

    PIOT-CDA-06-002

--- src/test/python/programmingtheiot/part03/integration/connection/MqttClientConnectorTest.py
+++ src/test/python/programmingtheiot/part03/integration/connection/MqttClientConnectorTest.py
@@ -45,7 +45,7 @@ class MqttClientConnectorTest(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	@unittest.skip("Ignore for now.")
+	#@unittest.skip("Ignore for now.")
 	def testConnectAndDisconnect(self):
 		delay = self.cfg.getInteger(ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE)
 		
