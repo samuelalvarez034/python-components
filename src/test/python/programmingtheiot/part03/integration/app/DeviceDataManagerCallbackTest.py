56c58605825ef5c12dca69ea3a8f4e31569479a5
samuelalvarez034 <you@example.com>
2025-05-27 10:45:10 +0200

    PIOT-CDA-10-002

--- src/test/python/programmingtheiot/part03/integration/app/DeviceDataManagerCallbackTest.py
+++ src/test/python/programmingtheiot/part03/integration/app/DeviceDataManagerCallbackTest.py
@@ -7,6 +7,7 @@
 # Copyright (c) 2020 by Andrew D. King
 # 
 
+
 import logging
 import unittest
 
@@ -17,26 +18,8 @@ import programmingtheiot.common.ConfigConst as ConfigConst
 from programmingtheiot.cda.app.DeviceDataManager import DeviceDataManager
 from programmingtheiot.data.ActuatorData import ActuatorData
 
-class DeviceDataManagerWithCommsTest(unittest.TestCase):
-	"""
-	This test case class contains very basic integration tests for
-	DeviceDataManager. It should not be considered complete,
-	but serve as a starting point for the student implementing
-	additional functionality within their Programming the IoT
-	environment.
-	
-	NOTE: This test MAY require the sense_emu_gui to be running,
-	depending on whether or not the 'enableEmulator' flag is
-	True within the ConstraineDevice section of PiotConfig.props.
-	If so, it must have access to the underlying libraries that
-	support the pisense module. On Windows, one way to do
-	this is by installing pisense and sense-emu within the
-	Bash on Ubuntu on Windows environment and then execute this
-	test case from the command line, as it will likely fail
-	if run within an IDE in native Windows.
-	
-	"""
-	
+class DeviceDataManagerCallbackTest(unittest.TestCase):
+
 	@classmethod
 	def setUpClass(self):
 		logging.basicConfig(format = '%(asctime)s:%(module)s:%(levelname)s:%(message)s', level = logging.DEBUG)
@@ -60,7 +43,19 @@ class DeviceDataManagerWithCommsTest(unittest.TestCase):
 		ddMgr.handleActuatorCommandMessage(actuatorData)
 		
 		sleep(10)
-		
+
+	def testActuatorDataCallback(self):
+		ddMgr = DeviceDataManager()
+		actuatorData = ActuatorData(typeID = ConfigConst.HVAC_ACTUATOR_TYPE)
+		actuatorData.setCommand(ConfigConst.COMMAND_ON)
+
+		ddMgr.handleActuatorCommandMessage(actuatorData)
+
+		sleep(10)
+
+	
 if __name__ == "__main__":
 	unittest.main()
 	
+
+
